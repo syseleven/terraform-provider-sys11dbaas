e87d29ad688a8a1f5e92d0a30eddfeebe4aa9ee7
Simon Pearce <s.pearce@syseleven.de>
2025-06-25 12:00:59 +0200

    update advanced example to use v2 endpoint

--- examples/postgresql/advanced/variables.tf
+++ examples/postgresql/advanced/variables.tf
@@ -49,7 +49,7 @@ variable "db_type" {
 
 variable "db_version" {
   type    = number
-  default = 17.2
+  default = 17.5
 }
 
 variable "db_disk_size" {
@@ -72,7 +72,22 @@ variable "db_backup_hour" {
   default = 4
 }
 
+variable "private_networking" {
+  type    = bool
+  default = true
+}
+
+variable "public_networking" {
+  type    = bool
+  default = false
+}
+
+variable "shared_subnet_cidr" {
+  type    = string
+  default = "10.245.0.0/24"
+}
+
 variable "db_remote_ips" {
   type    = list(string)
-  default = ["0.0.0.0/0"]
+  default = ["10.245.0.0/24"]
 }
