f679d0f839f4cbcda0a725bb2fa48ec052b570b3
Tobias Kohlbau <t.kohlbau@syseleven.de>
2026-09-16 13:27:49 +0200

    fix: do not set features if it's not present in the api response (#53)

--- internal/provider/database_resource.go
+++ internal/provider/database_resource.go
@@ -1334,8 +1334,6 @@ func psqlGetResponseToModel(ctx context.Context, db database.PostgreSQLGetRespon
 		var conversionDiags []diag.Diagnostic
 		applicationConfig.Features, conversionDiags = types.MapValueFrom(ctx, types.StringType, *db.ApplicationConfig.Features)
 		diags.Append(conversionDiags...)
-	} else {
-		applicationConfig.Features = types.MapNull(types.StringType)
 	}
 
 	model.Uuid = types.StringValue(db.Uuid)
